03640f3c40d81adf5623c25efb6d8231dfcf179a
Harsh saini <harshsaini555333@gmail.com>
2025-06-20 11:24:35 +0530

    Initial backend setup with Express and MongoDB connection

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
+const connectDB = require('./config/db')
+connectDB()
+
+const app =express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/',(req,res)=>{
+    res.send('Sheideo application is running')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`Server is running on localhost:${PORT}`)
+})
