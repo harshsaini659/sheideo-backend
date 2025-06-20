35d187bfc199ee1a8acc0eaa83b2d845f8f58574
Harsh saini <harshsaini555333@gmail.com>
2025-06-20 19:27:53 +0530

    Implement deleteProduct functionality

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {createProduct, getMyProducts, updateProduct} = require('../controllers/productController')
+const {createProduct, getMyProducts, updateProduct, deleteProduct} = require('../controllers/productController')
 const protect = require('../middleware/authMiddleware')
 
 router.post('/add', protect, createProduct)
 router.get('/mine', protect, getMyProducts)
-router.get('/:id', protect, updateProduct)
+router.put('/:id', protect, updateProduct)
+router.delete('/:id', protect, deleteProduct);
+
 
 module.exports = router
