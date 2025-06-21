50fc70ac6db7520fd7b3d62ed243bef8cb0be3e0
Harsh saini <harshsaini555333@gmail.com>
2025-06-21 19:14:18 +0530

    change file name of model

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require('../models/Product')
+const Product = require('../models/productModel')
 
 exports.createProduct = async (req, res) => {
   const { title, description, price, stock, category, images } = req.body
